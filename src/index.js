dbd787f18ed9fccca07736d16861547ed5f65db4
juroga_new <juanjrgast@gmail.com>
2023-10-09 20:26:57 +0200

    Basic html structure

--- src/index.js
+++ src/index.js
@@ -1,10 +1,1 @@
 
-function component() {
-    const element = document.createElement('div');
-
-    element.innerHTML = 'Hello webo';
-
-    return element;
-}
-
-document.body.appendChild(component());
